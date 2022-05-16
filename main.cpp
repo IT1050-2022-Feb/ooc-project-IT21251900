da22c560dd426912d15728426403f0dd96015deb
Avishka Umesh <99153285+IT21251214@users.noreply.github.com>
2022-05-16 05:19:07 +0000

    Updated main

--- main.cpp
+++ main.cpp
@@ -21,26 +21,31 @@ int main()
          << endl;
 	H1->DisplayHotelDetails();
 
-   // creating objects
-    Admin a1;
-    SiteSettings s1;
+  // creating objects
+    Admin *a1;
+    a1 = new Admin();
+    SiteSettings *s1;
+    s1 = new SiteSettings();
+
 
     // adding admin details
-    a1.AddAdminDetails(1, "admin@gmail.com", "admin22", "password");
+    a1->AddAdminDetails(1, "admin@gmail.com", "admin22", "password");
 
     // adding site details
-    s1.AddSiteDetails(1, 0777777777, "tugo@gmail.com", "no:21,address");
+    s1->AddSiteDetails(1, 0777777777, "tugo@gmail.com", "no:21,address");
 
     // displaying admin details
     cout << "Admin Details" << endl
          << endl;
-    a1.DisplayAdminDetails();
+    a1->DisplayAdminDetails();
 
     // displaying site details
     cout << endl
          << "Site Details" << endl
          << endl;
-    s1.DisplaySiteDetails();
+    s1->DisplaySiteDetails();
+
+  
 
 	return 0;
 }
